95ae6744d39ece143975cbfe5f82d5210042a730
Ghyferri Halfhide <ghyferri_04@hotmail.com>
2023-03-27 10:55:49 +0200

    added search function

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,23 @@
 import useNetwork from '@/data/network'
 import Link from 'next/link'
+import { useState } from 'react'
 
 export default function Home() {
+    const [filter, setFilter] = useState('')
     const { network, isLoading, isError } = useNetwork()
 
     if (isLoading) return <div>Loading...</div>
     if (isError) return <div>Error</div>
 
-    const stations = network.stations
+    const stations = network.stations.filter(station => station.name.toLowerCase().indexOf(filter.toLowerCase()) >= 0);
+
+    function handleFilterChange(e) {
+        setFilter(e.target.value);
+    }
+
     return (
         <div>
+            <input type="text" value={filter} onChange={handleFilterChange} />
             {stations.map(station => <Link href={`/stations/${station.id}`} key={station.id}>{station.name}</Link>)}
         </div>
     )
