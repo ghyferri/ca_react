717d9e7344acdca46468a324ca216768d36130f7
Ghyferri Halfhide <ghyferri_04@hotmail.com>
2023-03-27 10:25:30 +0200

    added fetcher and network settings base

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,38 +1,16 @@
-import Hello from '@/components/Hello'
-import styles from '@/styles/Home.module.css'
-import { useState } from 'react'
-
+import useNetwork from '@/data/network'
 
 
 export default function Home() {
-  const [count, setCount] = useState(0)
-  const [isVisible, setIsVisisble] = useState(false)
+    const { network, isLoading, isError } = useNetwork()
 
-  const names = ["Robert", "Lena", "Zebbi"]
-let namesClass = styles.isNotVisible
-if (isVisible){
-  namesClass = styles.isVisible
-}
-  function handleClick(){
-    setCount(count+1)
-    setIsVisisble(!isVisible)
-  }
-  return (
-   <div>
-    count: {count}
-    <button onClick={handleClick}>+1</button>
-    {isVisible ? ( <div>
-    {names.map((name) => <Hello name={name} key={name}/>)}
-    </div>)
-    : null
-    }
-    <div style={{display: isVisible ? 'block' : 'none'}} >
-    {names.map((name) => <Hello name={name} key={name}/>)}
-    </div>
-    <div className={namesClass}>
-    {names.map((name) => <Hello name={name} key={name}/>)}
-    </div>
+    if (isLoading) return <div>Loading...</div>
+    if (isError) return <div>Error</div>
 
- </div>
-  )
+    const stations = network.stations
+    return (
+        <div>
+            {stations.map(station => <p key={station.id}>{station.name}</p>)}
+        </div>
+    )
 }
