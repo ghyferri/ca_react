2ca8835fe5041f42bedc82292a4aedf167f49dd0
Ghyferri Halfhide <ghyferri_04@hotmail.com>
2023-04-24 09:35:31 +0200

    added map images

---
+++ src/data/image.js
@@ -0,0 +1,19 @@
+import fetcher from "./_fetcher";
+import useSWR from "swr";
+
+export default function useImage(station) {
+  const { data, error, isLoading } = useSWR(
+    `https://graph.mapillary.com/images?access_token=${
+      process.env.NEXT_PUBLIC_MAPILLARY_TOKEN
+    }&fields=id,thumb_1024_url&bbox=${station.longitude - 0.0001},${
+      station.latitude - 0.0001
+    },${station.longitude + 0.0001},${station.latitude + 0.0001}&limit=1`,
+    fetcher
+  );
+  console.log(data);
+  return {
+    image: data?.data[0]?.thumb_1024_url,
+    isLoading,
+    isError: error,
+  };
+}
--- src/pages/stations/[stationId].js
+++ src/pages/stations/[stationId].js
@@ -1,15 +1,18 @@
-import useNetwork from '@/data/network';
-import { useRouter } from 'next/router'
+import useNetwork from "@/data/network";
+import { useRouter } from "next/router";
+import StationImage from "@/components/StationImage";
 
 export default function Home() {
-  const { network, isLoading, isError } = useNetwork()
-  const router = useRouter()
- 
-  if (isLoading) return <div>Loading...</div>
-  if (isError) return <div>Error</div>
+  const { network, isLoading, isError } = useNetwork();
+  const router = useRouter();
 
-  const station = network.stations.find(station => station.id === router.query.stationId)
-  console.log(station)
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>Error</div>;
+
+  const station = network.stations.find(
+    (station) => station.id === router.query.stationId
+  );
+  console.log(station);
   return (
     <div>
       <h1>{station.name}</h1>
@@ -18,6 +21,8 @@ export default function Home() {
       <p>empty slots: {station.empty_slots}</p>
       <p>latitude: {station.latitude}</p>
       <p>longitude: {station.longitude}</p>
+
+      <StationImage station={station} />
     </div>
-  )
+  );
 }
